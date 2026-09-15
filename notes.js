47a9c42a32b0d69ee638727e7f0d848e5a974c3c
Ayush14122005 <ayushsharma141205@gmail.com>
2026-09-15 11:13:30 +0530

    asynchronus

--- notes.js
+++ notes.js
@@ -1,6 +1,21 @@
 // Synchronus is a blocking architecture, so the execution of each operation is dependent on the completion of the one before it.
+// function sum(){
+//     console.log("2");
+// }
+// function example(){
+//     console.log("1");
+//     sum();
+//     console.log("3");
+// }
+// example();
+
+
+// Asynchronus is a non blocking architecture, so the execution of one task isn't dependent on another .
+// Task can run simultaneously
 function sum(){
-    console.log("2");
+    setTimeout(() =>{
+        console.log("2");
+    },2000);
 }
 function example(){
     console.log("1");
@@ -8,7 +23,3 @@ function example(){
     console.log("3");
 }
 example();
-
-
-// Asynchronus is a non blocking architecture, so the execution of one task isn't dependent on another .
-// Task can run simultaneously
